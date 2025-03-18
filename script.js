fc5a190f62c31e67b548c031aee01f4ceec854a1
FlaviaCristinaS <silva4.flavia@gmail.com>
2025-03-17 23:22:46 -0300

    html/js finalizados

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-function insert(mum){
+function insert(num){
 
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
 
 function limpar(){
+
     document.getElementById('resultado').innerHTML = "";
 }
 
 function deletar(){
-    var resultado = document.getElementById('resultado').innerHTML;        // ver todos os valores que estão dentro de "resultdado" e apagar um
-    document.getElementById('resultado');innerHTML = resultado.substring(0, resultado.length -1);
+
+    var resultado = document.getElementById('resultado').innerHTML;  
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1); // ver todos os valores que estão dentro de "resultdado" e apagar um
 }
 
 function calcular(){
+
     var resultado = document.getElementById('resultado').innerHTML;
+
     if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado); //
+        document.getElementById('resultado').innerHTML = eval(resultado); // executa a expressão matemática
     }
     else{
         document.getElementById('resultado').innerHTML = ""; 
